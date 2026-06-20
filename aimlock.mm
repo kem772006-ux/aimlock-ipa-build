82be596610e2e30cd9bf25943daebe0d9879ef6f
kem772006-ux <kem772006@gmail.com>
2026-06-20 19:02:40 +0700

    Update aimlock.mm

--- aimlock.mm
+++ aimlock.mm
@@ -1,5 +1,3 @@
-// aimlock.mm - AppDelegate без SceneDelegate
-// Компиляция: clang++ -arch arm64 -framework UIKit -framework Foundation
 #import <UIKit/UIKit.h>
 #import <mach/mach.h>
 #import <sys/sysctl.h>
@@ -12,15 +10,12 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // 1. Создаем окно ВРУЧНУЮ (без сцен)
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor blackColor];
     
-    // 2. Создаем контроллер
     UIViewController *vc = [[UIViewController alloc] init];
     vc.view.backgroundColor = [UIColor blackColor];
     
-    // 3. Добавляем статусную метку (СРАЗУ)
     self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 100, self.window.bounds.size.width - 40, 400)];
     self.statusLabel.text = @"⏳ AIMLOCK\n\nDang khoi dong...\nVui long doi...";
     self.statusLabel.textColor = [UIColor orangeColor];
@@ -29,7 +24,6 @@
     self.statusLabel.numberOfLines = 12;
     [vc.view addSubview:self.statusLabel];
     
-    // 4. Кнопка Retry
     UIButton *retryBtn = [UIButton buttonWithType:UIButtonTypeSystem];
     retryBtn.frame = CGRectMake(60, 520, self.window.bounds.size.width - 120, 50);
     [retryBtn setTitle:@"THU LAI" forState:UIControlStateNormal];
@@ -42,7 +36,6 @@
     self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
     
-    // 5. Запускаем поиск игры с задержкой 0.5 сек
     [self performSelector:@selector(startFindGame) withObject:nil afterDelay:0.5];
     
     return YES;
@@ -55,21 +48,26 @@
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
         pid_t foundPid = -1;
         
-        // Перебор PID от 1 до 9999 (быстрее чем sysctl KERN_PROC_ALL)
-        for (pid_t i = 1; i < 9999; i++) {
-            char name[256] = {0};
-            proc_name(i, name, sizeof(name));
-            if (strlen(name) > 0) {
-                NSString *procName = [NSString stringWithUTF8String:name];
-                if ([procName localizedCaseInsensitiveContainsString:@"PUBGM"] ||
-                    [procName localizedCaseInsensitiveContainsString:@"ShadowTrackerExtra"] ||
-                    [procName localizedCaseInsensitiveContainsString:@"codm"] ||
-                    [procName localizedCaseInsensitiveContainsString:@"freefire"] ||
-                    [procName localizedCaseInsensitiveContainsString:@"bgmi"]) {
-                    foundPid = i;
-                    break;
+        int mib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0};
+        size_t size = 0;
+        
+        if (sysctl(mib, 4, NULL, &size, NULL, 0) == 0) {
+            struct kinfo_proc *procList = (struct kinfo_proc *)malloc(size);
+            if (sysctl(mib, 4, procList, &size, NULL, 0) == 0) {
+                size_t count = size / sizeof(struct kinfo_proc);
+                for (size_t i = 0; i < count; i++) {
+                    NSString *name = [NSString stringWithUTF8String:procList[i].kp_proc.p_comm];
+                    if ([name localizedCaseInsensitiveContainsString:@"PUBGM"] ||
+                        [name localizedCaseInsensitiveContainsString:@"ShadowTrackerExtra"] ||
+                        [name localizedCaseInsensitiveContainsString:@"codm"] ||
+                        [name localizedCaseInsensitiveContainsString:@"freefire"] ||
+                        [name localizedCaseInsensitiveContainsString:@"bgmi"]) {
+                        foundPid = procList[i].kp_proc.p_pid;
+                        break;
+                    }
                 }
             }
+            free(procList);
         }
         
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -79,7 +77,6 @@
             } else {
                 mach_port_t taskPort = MACH_PORT_NULL;
                 kern_return_t kr = task_for_pid(mach_task_self(), foundPid, &taskPort);
-                
                 if (kr == KERN_SUCCESS) {
                     self.statusLabel.text = [NSString stringWithFormat:@"✅ KET NOI THANH CONG!\n\nPID: %d\nTask: 0x%x\n\nQUAY LAI GAME NGAY!\nAimlock dang hoat dong.", foundPid, taskPort];
                     self.statusLabel.textColor = [UIColor greenColor];
